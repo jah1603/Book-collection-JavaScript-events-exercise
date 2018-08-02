28b166646dad28e2db9f5531be26fc300c970c75
James Henderson <hendersonjamesalexander@gmail.com>
2018-08-02 14:32:41 +0100

    second task complete

--- js/app.js
+++ js/app.js
@@ -6,4 +6,11 @@ document.addEventListener('DOMContentLoaded', () => {
     const resultText = document.querySelector('#title-result')
     resultText.textContent = `You typed ${event.target.value}.`;
   })
+
+  const authorInput = document.querySelector('#author');
+  authorInput.addEventListener('input', (event) =>{
+    const resultAuthor = document.querySelector('#author-result')
+    resultAuthor.textContent = `You typed ${event.target.value}.`
+  });
+
 })
